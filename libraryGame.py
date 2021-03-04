42bd4a8a438bfe46cf00cdc9ff85c183a5e1e8b8
Arthur Arnx <arthur.arnx@gmail.com>
2021-03-04 16:03:25 +0100

    libraryGame

---
+++ libraryGame.py
@@ -0,0 +1,10 @@
+impôrt LibraryNeuralNetwork.py as lnn
+
+class Car():
+    def __init__(self):
+        self.acceleration = 0
+        self.orientation = 90
+        self.x = 350
+        self.y = 150
+        self.distance = 0
+        self.nn = lnn.NeuralNetwork(size=[2,5,5,4])
