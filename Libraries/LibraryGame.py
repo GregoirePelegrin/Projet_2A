ae650bc2b00d99515b97f096366927ed321c3214
Arthur Arnx <arthur.arnx@gmail.com>
2021-03-24 10:28:53 +0100

    game

---
+++ Libraries/LibraryGame.py
@@ -0,0 +1,41 @@
+import Libraries.LibraryNeuralNetwork as lnn
+
+class Car():
+    counter = 0
+    def __init__(self, _id=None):
+        if _id != None:
+            self.id = _id
+        else:
+            self.id = Car.counter
+        self.alive = True
+        self.speed = 10
+        self.orientation = 90
+        self.x = 350
+        self.y = 80
+        self.lastX = 0
+        self.lastY = 0
+        self.totalDistance = 0
+        self.nn = lnn.NeuralNetwork(size=lnn.NN_SIZE)
+        self.nextCheckpoint = 2
+        self.orientedCarImg = None
+        self.checkpointPassedCounter = 0
+        Car.counter += 1
+    def __str__(self):
+        return "Car({})".format(self.id)
+
+    def reinitialization(self):
+        self.alive = True
+        self.acceleration = 0
+        self.orientation = 80
+        self.x = 350
+        self.y = 90
+        self.nn.fitness = 0
+        self.totalDistance = 0
+        self.nextCheckpoint = 2
+        self.checkpointPassedCounter = 0
+
+    def copy(self):
+        new_car = Car()
+        new_car.nn = self.nn
+        new_car.orientedCarImg = self.orientedCarImg
+        return new_car
---
+++ game.py
@@ -0,0 +1,208 @@
+import pygame
+import os
+import math
+import time
+import random
+
+import Libraries.LibraryGame as lg
+import Libraries.collisionDetection as cd
+
+THRESHOLD = 0.55
+NB_CAR = 100
+NB_GENERATION = 500
+TIME_RACE = 60
+
+pygame.init()
+gameDisplay = pygame.display.set_mode((800,600))
+pygame.display.set_caption("Racing")
+clock = pygame.time.Clock()
+
+carImg = pygame.image.load("imgs/car.png").convert_alpha()
+
+cars = []
+for i in range(NB_CAR):
+    car = lg.Car(i)
+    car.orientedCarImg = pygame.transform.rotate(carImg, 90)
+    cars.append(car)
+
+VIEW_MODE = True
+
+finished = False
+
+drawing = False
+points = []
+
+### TO NUMPY
+exterior = [(231, 67), (370, 57), (502, 59), (602, 81), (625, 99), (635, 132), (635, 165), (622, 214), (619, 243), (629, 274), (647, 305), (654, 343), (652, 363), (647, 382), (637, 402), (620, 426), (605, 452), (592, 473), (567, 485), (544, 490), (517, 490), (494, 486), (467, 478), (461, 471), (442, 465), (422, 463), (399, 473), (385, 487), (365, 508), (347, 528), (310, 548), (284, 552), (256, 554), (229, 548), (214, 534), (202, 514), (182, 502), (174, 490), (161, 462), (154, 442), (152, 415), (151, 383), (150, 352), (142, 337), (133, 318), (119, 302), (101, 282), (92, 258), (91, 233), (96, 210), (104, 182), (114, 161), (125, 135), (137, 110), (158, 91), (178, 79), (205, 72), (230, 68)]
+pygame.draw.lines(gameDisplay, (255,255,255), False, exterior)
+interior = [(303, 100), (345, 98), (392, 96), (442, 96), (479, 100), (514, 106), (550, 118), (567, 130), (571, 142), (574, 158), (570, 173), (567, 198), (564, 216), (565, 230), (570, 256), (576, 278), (581, 286), (587, 304), (593, 329), (598, 346), (598, 362), (594, 373), (582, 390), (574, 402), (561, 412), (538, 421), (515, 420), (492, 416), (466, 412), (453, 413), (431, 418), (409, 424), (394, 428), (370, 442), (353, 459), (334, 474), (294, 485), (269, 485), (253, 477), (239, 466), (230, 450), (222, 423), (215, 400), (212, 381), (202, 351), (194, 321), (180, 295), (166, 270), (154, 245), (154, 217), (159, 198), (168, 178), (184, 155), (202, 141), (225, 123), (243, 110), (275, 102), (302, 100), (302, 100)]
+pygame.draw.lines(gameDisplay, (255,255,255), False, interior)
+
+
+gen = 0
+while gen < NB_GENERATION and not finished :
+    timeGen = time.time()
+    print("Gen " + str(gen))
+
+    timeRace = 0
+    while timeRace < TIME_RACE:
+        tokenStop = True
+
+        for event in pygame.event.get():
+            if(event.type == pygame.QUIT):
+                pygame.quit()
+            if(event.type == pygame.KEYDOWN):
+                if event.key == pygame.K_v :
+                    VIEW_MODE = not VIEW_MODE
+                if event.key == pygame.K_e :
+                    gen = NB_GENERATION
+            if(event.type == pygame.MOUSEBUTTONDOWN):
+                points.append(event.pos)
+                drawing = True
+        
+        ######## Testing intersection by mouse
+        if drawing and len(points) > 1:
+            gameDisplay.fill((0,0,0))
+            pygame.draw.lines(gameDisplay, (255,255,255), False, interior)
+            pygame.draw.lines(gameDisplay, (255,255,255), False, exterior)
+            points[-1] = pygame.mouse.get_pos()
+            r = None
+            for p in range(len(interior)-1):
+                r = cd.calculateIntersectPoint([points[-2][0], points[-2][1]], [points[-1][0], points[-1][1]],
+                                        [interior[p][0], interior[p][1]], [interior[p+1][0], interior[p+1][1]])
+                if r is not None :
+                    pygame.draw.lines(gameDisplay, (255,0,0), False, points[-2:])
+                    pygame.draw.circle(gameDisplay,(255,0,0),(r[0], r[1]), 2)
+                    break
+            if r is None :
+                for p in range(len(exterior)-1):
+                    r = cd.calculateIntersectPoint([points[-2][0], points[-2][1]], [points[-1][0], points[-1][1]],
+                                            [exterior[p][0], exterior[p][1]], [exterior[p+1][0], exterior[p+1][1]])
+                    if r is not None :
+                        pygame.draw.lines(gameDisplay, (255,0,0), False, points[-2:])
+                        pygame.draw.circle(gameDisplay,(255,0,0),(r[0], r[1]), 2)
+                        break
+            if r is None:
+                pygame.draw.lines(gameDisplay, (0,255,0), False, points[-2:])
+        ########
+
+        
+        VIEW_MODE and gameDisplay.fill((0,0,0))
+        VIEW_MODE and pygame.draw.lines(gameDisplay, (255,255,255), False, interior)
+        VIEW_MODE and pygame.draw.lines(gameDisplay, (255,255,255), False, exterior)
+
+        for car in cars :
+            if(not car.alive):
+                continue
+
+            ######## Drawing  and views calculations
+            car.orientedCarImg = pygame.transform.rotate(carImg, -car.orientation)
+            new_rect = car.orientedCarImg.get_rect(center = (car.x,car.y))
+            VIEW_MODE and gameDisplay.blit(car.orientedCarImg, new_rect.topleft)
+
+            lineSurface = pygame.Surface((800,600), pygame.SRCALPHA, 32)
+            lineSurface = lineSurface.convert_alpha()
+
+            dx1 = math.cos((car.orientation-90) * math.pi / 180)
+            dy1 = math.sin((car.orientation-90) * math.pi / 180)
+            dx2 = math.cos((car.orientation-45) * math.pi / 180)
+            dy2 = math.sin((car.orientation-45) * math.pi / 180)
+            dx3 = math.cos((car.orientation-135) * math.pi / 180)
+            dy3 = math.sin((car.orientation-135) * math.pi / 180)
+
+            dist1 = None
+            dist2 = None
+            dist3 = None
+            for p in range(len(interior)-1):
+                if dist1 is None :
+                    dist1 = cd.calculateIntersectPoint([car.x, car.y], [car.x+dx1*50,car.y+dy1*50],
+                                            interior[p], interior[p+1])
+                if dist2 is None :
+                    dist2 = cd.calculateIntersectPoint([car.x, car.y], [car.x+dx2*50,car.y+dy2*50],
+                                        [interior[p][0], interior[p][1]], [interior[p+1][0], interior[p+1][1]])
+                if dist3 is None :
+                    dist3 = cd.calculateIntersectPoint([car.x, car.y], [car.x+dx3*50,car.y+dy3*50],
+                                        [interior[p][0], interior[p][1]], [interior[p+1][0], interior[p+1][1]])
+
+            for p in range(len(exterior)-1):
+                if dist1 is None :
+                    dist1 = cd.calculateIntersectPoint([car.x, car.y], [car.x+dx1*50,car.y+dy1*50],
+                                        [exterior[p][0], exterior[p][1]], [exterior[p+1][0], exterior[p+1][1]])
+                if dist2 is None :
+                    dist2 = cd.calculateIntersectPoint([car.x, car.y], [car.x+dx2*50,car.y+dy2*50],
+                                        [exterior[p][0], exterior[p][1]], [exterior[p+1][0], exterior[p+1][1]])
+                if dist3 is None :
+                    dist3 = cd.calculateIntersectPoint([car.x, car.y], [car.x+dx3*50,car.y+dy3*50],
+                                        [exterior[p][0], exterior[p][1]], [exterior[p+1][0], exterior[p+1][1]])
+
+            if dist1 is None :
+                dist1 = 50
+            else :
+                dist1 = math.sqrt( (car.x-dist1[0])**2 + (car.y-dist1[1])**2 )
+                if dist1 < 8 :
+                    car.alive = False
+            if dist2 is None :
+                dist2 = 50
+            else :
+                dist2 = math.sqrt( (car.x-dist2[0])**2 + (car.y-dist2[1])**2 )
+                if dist2 < 8 :
+                    car.alive = False
+            if dist3 is None :
+                dist3 = 50
+            else :
+                dist3 = math.sqrt( (car.x-dist3[0])**2 + (car.y-dist3[1])**2 )
+                if dist3 < 8 :
+                    car.alive = False
+            if(not car.alive):
+                continue
+
+            if dist1 < 50 :
+                VIEW_MODE and pygame.draw.line(lineSurface, (255,0,0),(car.x,car.y),(car.x+dx1*50,car.y+dy1*50))
+            else:
+                VIEW_MODE and pygame.draw.line(lineSurface, (0,255,0),(car.x,car.y),(car.x+dx1*50,car.y+dy1*50))
+            if dist2 < 50 :
+                VIEW_MODE and pygame.draw.line(lineSurface, (255,0,0),(car.x,car.y),(car.x+dx2*50,car.y+dy2*50))
+            else:
+                VIEW_MODE and pygame.draw.line(lineSurface, (0,255,0),(car.x,car.y),(car.x+dx2*50,car.y+dy2*50))
+            if dist3 < 50 :
+                VIEW_MODE and pygame.draw.line(lineSurface, (255,0,0),(car.x,car.y),(car.x+dx3*50,car.y+dy3*50))
+            else:
+                VIEW_MODE and pygame.draw.line(lineSurface, (0,255,0),(car.x,car.y),(car.x+dx3*50,car.y+dy3*50))
+
+            VIEW_MODE and gameDisplay.blit(car.orientedCarImg, new_rect.topleft)
+            VIEW_MODE and gameDisplay.blit(lineSurface, (0,0))
+            ########
+
+            car.x += dx1*car.speed
+            car.y += dy1*car.speed
+
+            ######## Neural network inputs
+            listInput = car.nn.evaluate([car.speed, dist1, dist2, dist3])
+            #listInput = [random.uniform(0, 1),random.uniform(0, 1)]
+            
+            if(listInput[0] >= THRESHOLD):
+                car.orientation -= 5
+
+            if(listInput[1] >= THRESHOLD):
+                car.orientation += 5
+            #########
+
+            if car.x != car.lastX or car.y != car.lastY:
+                tokenStop = False
+        
+        if tokenStop:
+            break
+        pygame.display.update()
+        clock.tick(30)
+        timeRace += 1
+        # temp_data = ga.evolve(cars)
+
+    for car in cars :
+        car.alive = True
+        car.speed = 10
+        car.orientation = 90
+        car.x = 350
+        car.y = 80
+    gen += 1
+
+print(points)
