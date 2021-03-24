c570111a0bc4f076b1b3058a95b24cf6e3f55287
TruiteAgitee <pelegrin.gregoire@gmail.com>
2021-03-24 12:43:22 +0100

    Neuron class terminated
    * Adding populate() method
    * Adding evaluate() method
    * Tested

--- Libraries/LibraryNeuralNetwork.py
+++ Libraries/LibraryNeuralNetwork.py
@@ -3,25 +3,38 @@ from random import uniform
 import math
 import numpy as np
 
-# Neural Network related constants
+# Constants
 NEURAL_NETWORK_SIZE = [5]
 
+# Activation functions
+def relu(x):
+    return max(0, x)
+def sigmoid(x):
+    return 1/(1+math.exp(-x))
+
+# Classes
 class Neuron:
     objectCounter = 0
-    def __init__(self, neuron=None):
+    def __init__(self, dimension=0, neuron=None):
         self.id = Neuron.objectCounter
         self.value = 0
 
         if neuron == None:
-            self.biaises = np.array([])
-            self.weights = np.array([])
-            # TODO: Generate random weights and biaises
+            self.populate(dimension)
         else:
-            self.biaises = neuron.biaises.copy()
+            self.bias = neuron.bias
             self.weights = neuron.weights.copy()
 
         Neuron.objectCounter += 1
     def __str__(self):
-        return "Neuron({})".format(self.id)
+        return "Neuron(id={}, value={})".format(self.id, self.value)
     def __repr__(self):
-        return "Neuron({}\n\t{}\n\t{})".format(self.id, self.biaises, self.weights)
+        return "Neuron(id={}\n\tbias={}\n\tweights={})".format(self.id, self.bias, self.weights)
+
+    def evaluate(self, inputs):
+        self.value = sigmoid(np.sum(self.weights * inputs) + self.bias)
+    def populate(self, size):
+        self.bias = uniform(-1, 1)
+        self.weights = 2*np.random.rand(size)-1
+# TODO: Layer class
+# TODO: NeuralNetwork class
